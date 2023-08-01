38b62121c30452032a4c2cd429d78fd71b59dcf4
Gaston Milano <gmilano@genexus.com>
2023-07-31 23:28:05 -0300

    Fix redirect after login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import { MsalProvider, useMsal, useIsAuthenticated } from "@azure/msal-react";
 import { AZURE_AD_CLIENT_ID, AZURE_AD_TENANT_ID_URL } from '@/utils/app/const';
 
 import '@/styles/globals.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoginComponent from '@/components/Auth/Login';
 
 
@@ -18,32 +18,39 @@ const inter = Inter({ subsets: ['latin'] });
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
   const [isAuthenticated, setIsAuthenticated] = useState(useIsAuthenticated());
+  const [msalInstance, setMsalInstance] = useState<PublicClientApplication|null>(null);
 
   const handleLogin = () => {
     setIsAuthenticated(true);
   }
 
+  useEffect(() => {
+
+  
   const msalInstance = new PublicClientApplication({
     auth: {
         clientId: AZURE_AD_CLIENT_ID,
         authority: AZURE_AD_TENANT_ID_URL,
-        redirectUri: "http://localhost:3000",
+        redirectUri: window.location.origin,
     },
     cache: {
         cacheLocation: "localStorage", // This configures where your cache will be stored
         storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
     }
-  });
+    });
+    setMsalInstance(msalInstance);
+  }, []);
   
   return (
-    <MsalProvider instance={msalInstance}>
+    (msalInstance ?  <MsalProvider instance={msalInstance}>
       <div className={inter.className}>
         <Toaster />       
         <QueryClientProvider client={queryClient}>
           {isAuthenticated ? <Component {...pageProps} /> : <LoginComponent onLogin={handleLogin}/>}
         </QueryClientProvider>
       </div>
-    </MsalProvider>
+    </MsalProvider> : null)
+   
   );
 }
 
